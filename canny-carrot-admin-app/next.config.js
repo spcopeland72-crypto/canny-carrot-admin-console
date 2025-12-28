deb173a1b3c3767900035b31aa910e1efdb7d5b8
auroxeon <copeland_simon@yahoo.co.uk>
2025-12-28 21:13:33 +0000

    Improve webpack config - Use IgnorePlugin for react-native instead of ignore-loader - Better webpack integration

--- canny-carrot-admin-app/next.config.js
+++ canny-carrot-admin-app/next.config.js
@@ -2,20 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   // Exclude React Native components from build (they're Expo leftovers, not used in Next.js app)
-  webpack: (config) => {
+  webpack: (config, { webpack }) => {
     // Ignore react-native imports - these components aren't used by Next.js app
     config.resolve.alias = {
       ...config.resolve.alias,
       'react-native': false,
     };
     
-    // Ignore files in src/components (React Native components)
-    config.module = config.module || {};
-    config.module.rules = config.module.rules || [];
-    config.module.rules.push({
-      test: /src\/components\/.*\.(tsx?|jsx?)$/,
-      use: 'ignore-loader',
-    });
+    // Use IgnorePlugin to prevent webpack from processing react-native
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^react-native$/,
+      })
+    );
     
     return config;
   },
